f4d7c158bb61eb14ccaeb9a12ecaac2fac650330
josmargui <joseantoniomartin.87@gmail.com>
2022-08-16 02:13:01 +0200

    timeline education and experience

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,29 +1,56 @@
 import React from "react";
 import { FaGraduationCap } from 'react-icons/fa'
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css';
+import './education.scss'
+
 
 const Education = ({ education }) => {
     return(
         <div>
-            <div>
+            <div className="block-title">
                 <h2><FaGraduationCap/> Formación</h2>
             </div>
+            <VerticalTimeline>
             {education.map((study) => {
-
+                
                     return (
                         <div>
-                            <p>
-                                {study.name}
-                            </p>
-                            <p>
-                                {study.date}
-                            </p>
-                            <p>
-                                {study.where}
-                            </p>
+                            <VerticalTimelineElement key={study.key} lineColor={ 'red' } className="vertical-timeline-element--work" 
+                                contentStyle={{ background: '#153840', color: 'white', marginBlock:'35px' }}
+                                contentArrowStyle={{ borderRight: '7px solid  #153840' }}
+                                date={study.date} dateClassName={"dateString"}  iconStyle={{ background: '#153840', color: '#fff' }}
+                                icon={<FaGraduationCap/>}>
+                                <h3 className="vertical-timeline-element-title">
+                                    {study.name}
+                                </h3>
+                                <p>
+                                    {study.where}
+                                </p>
+                            </VerticalTimelineElement>
                         </div>
                     );
+                    
                 })
             }
+            
+            {/* <VerticalTimelineElement
+                className="vertical-timeline-element--education"
+                date="2002 - 2006"
+                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                icon={<FaGraduationCap />}
+            >
+                <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+                <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+                <p>
+                Creative Direction, Visual Design
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                icon={<FaGraduationCap />}
+            /> */}
+            </VerticalTimeline>
         </div>
     );
 }
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import { FaToolbox } from 'react-icons/fa'
+import { VerticalTimeline, VerticalTimelineElement  } from "react-vertical-timeline-component";
 import './experience.scss'
+import 'react-vertical-timeline-component/style.min.css';
+
 const Experience = ({ experience }) => {
     return(
         <div>
             <div>
                 <h2><FaToolbox/> Experiencia</h2>
             </div>
-            {experience && experience.map((exp) => {
-                return( 
-                    <div className="experience-block">
-                        <div className="experience-block__item">
-                            <p>{exp.where}</p>
-                            <p>{exp.date}</p>
+            <VerticalTimeline>
+                {experience && experience.map((exp) => {
+                    return( 
+                        <div className="experience-block">
+                        <VerticalTimelineElement className="vertical-timeline-element--work"
+                            contentStyle={{ background: '#153840', color: 'aquamarine', marginBlock:'35px' }}
+                            contentArrowStyle={{ borderRight: '7px solid  #153840' }}
+                            date={exp.date} dateClassName={"dateString" }iconStyle={{ background: '#153840', color: '#fff' }}
+                            icon={<FaToolbox/>}>
+                            <div className="experience-block__item">
+                                <p>{exp.where}</p>
+                                <p>{exp.date}</p>
+                            </div>
+                            <div className="experience-block__itemDescription">
+                                <p>{exp.name}</p>
+                                <p>{exp.description}</p>
+                            </div>
+                        </VerticalTimelineElement>
                         </div>
-                        <div className="experience-block__itemDescription">
-                            <p>{exp.name}</p>
-                            <p>{exp.description}</p>
-                        </div>
-                        
-                    </div>
-                    )})
-            }
+                        )})
+                }
+            </VerticalTimeline>
         </div>
     );
 
